16ee6b1e1b6faca35400c2c6106f4355ad81aa2c
aramkoukia <aramkoukia@gmail.com>
2014-08-12 19:52:40 -0700

    Added the Exception Filter

---
+++ Attractions.Service/Attractions.Service/Filters/ExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Attractions.Service.Filters
+{
+    public class ExceptionFilter : IExceptionFilter 
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            var loggingConfig = new Microsoft.Practices.EnterpriseLibrary.Logging.LoggingConfiguration();
+            var logWriter = new Microsoft.Practices.EnterpriseLibrary.Logging.LogWriter(loggingConfig);
+            logWriter.Write(filterContext.Exception);
+        }
+    }
+}
