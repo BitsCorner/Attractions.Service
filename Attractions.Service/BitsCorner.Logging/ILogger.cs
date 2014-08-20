6f82472cf561f4de14db129b14e1e90509bcee06
aramkoukia <aramkoukia@gmail.com>
2014-08-19 19:15:32 -0700

    Added Logging project

---
+++ Attractions.Service/BitsCorner.Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsCorner.Logging
+{
+    public interface ILogger
+    {
+        Task Log(string message, LogType logType);
+        Task Log(Exception exception);
+    }
+
+    public enum LogType
+    {
+        Verbose,
+        Information,
+        Warning,
+        Error
+    }
+}
---
+++ Attractions.Service/BitsCorner.Logging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsCorner.Logging
+{
+    public class Logger : ILogger
+    {
+        public Task Log(string message, LogType logType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Log(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
