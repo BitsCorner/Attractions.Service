6044c7129e7427d5a4a5aab28aab9f93ec4a0edf
aramkoukia <aramkoukia@gmail.com>
2014-09-15 14:55:26 -0700

    Added Flickr and YouTube interfaces

---
+++ Attractions.Service/Attractions.Repository/IYouTubeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attractions.Repository
+{
+    public interface IYouTubeRepository
+    {
+
+    }
+}
