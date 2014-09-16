dfad92e3461fa6540d036968df2ad86e0ada93a0
aramkoukia <aramkoukia@gmail.com>
2014-09-15 21:31:31 -0700

    Flickr Repository and Integration Tests

---
+++ Attractions.Service/Attractions.Repository/FlickrManager.cs
@@ -0,0 +1,64 @@
+﻿using FlickrNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attractions.Repository
+{
+    public class FlickrManager
+    {
+        public const string ApiKey = "81bc973400e23dc8453fe79eb69a6250";
+        public const string SharedSecret = "06c3086edefa2cb2";
+
+        public static Flickr GetInstance()
+        {
+            return new Flickr(ApiKey, SharedSecret);
+        }
+
+        public static Flickr GetAuthInstance()
+        {
+            var f = new Flickr(ApiKey, SharedSecret);
+            if (OAuthToken != null)
+            {
+                f.OAuthAccessToken = OAuthToken.Token;
+                f.OAuthAccessTokenSecret = OAuthToken.TokenSecret;
+            }
+            return f;
+        }
+
+        public static OAuthAccessToken OAuthToken
+        {
+            get
+            {
+                //if (HttpContext.Current.Request.Cookies["OAuthToken"] == null)
+                //{
+                    return null;
+                //}
+                //var values = HttpContext.Current.Request.Cookies["OAuthToken"].Values;
+                //return new OAuthAccessToken
+                //{
+                //    FullName = values["FullName"],
+                //    Token = values["Token"],
+                //    TokenSecret = values["TokenSecret"],
+                //    UserId = values["UserId"],
+                //    Username = values["Username"]
+                //};
+            }
+            set
+            {
+                //var cookie = new HttpCookie("OAuthToken")
+                //{
+                //    Expires = DateTime.UtcNow.AddDays(1),
+                //};
+                //cookie.Values["FullName"] = value.FullName;
+                //cookie.Values["Token"] = value.Token;
+                //cookie.Values["TokenSecret"] = value.TokenSecret;
+                //cookie.Values["UserId"] = value.UserId;
+                //cookie.Values["Username"] = value.Username;
+                //HttpContext.Current.Response.AppendCookie(cookie);
+            }
+        }
+    }
+}
--- Attractions.Service/Attractions.Repository/IFlickrRepository.cs
+++ Attractions.Service/Attractions.Repository/IFlickrRepository.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Attractions.Repository
 {
     public interface IFlickrRepository
     {
-        Task AddPicture();
-        Task UpdatePicture();
-        Task GetPictures();
+        string AddPicture(Stream stream, string fileName, string title, string description, string tags, bool isPublic, bool isFamily, bool isFriend);
+        FlickrNet.PhotoInfo GetPictures();
         Task AddPictureSet();
     }
 }
